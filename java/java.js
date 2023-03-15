a81fa36db58cac8de47826821aeb797e24d75aa7
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-03-15 20:41:12 +0100

    nmk

--- java/java.js
+++ java/java.js
@@ -5,4 +5,11 @@ let sliderSectionLast = sliderSection[sliderSection.length -1];
 const btnLeft = document.querySelector("btn-left");
 const btnRight = document.querySelector("btn-right");
 
-slider.insertAdjacentElement('afterbegin, sliderSectionLast')
+slider.insertAdjacentElement('afterbegin, sliderSectionLast');
+
+function Next()
+{
+    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+}
