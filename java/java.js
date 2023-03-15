6ebd70c42af23111acb3a54d3ef6377b19f25c2d
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-03-15 20:47:32 +0100

    bbbb

--- java/java.js
+++ java/java.js
@@ -12,4 +12,13 @@ function Next()
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 500);
 }
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
