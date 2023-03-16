0a106dd283d460cf35e8a3d14117e0a694928aa9
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-03-16 03:06:35 +0100

    wsdfg

--- java.js
+++ java.js
@@ -1,7 +1,9 @@
 var counter = 1;
 
-setInterval(function)
-{
-    document.getElementsById('radio' + counter).checked = true;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
     counter++;
-    if (counter>4){counter=1}, 7000}
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 7000);
