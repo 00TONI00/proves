ab76fb4e1d1aab07cbf4ec0e444562e48049c38a
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-03-17 13:01:00 +0100

    ezsdxfcgvhbjklm

--- java.js
+++ java.js
@@ -1,19 +1,33 @@
-var animacionMostrada = false;
+var currentSlide = 0;
+var sliderItems = document.querySelectorAll(".slider-item");
 
-function cargarPagina() {
-  if (!animacionMostrada) {
-    // Mostrar la pantalla de espera con la animación
-    document.getElementById("pantallaEspera").style.display = "block";
-    
-    // Cambiar la variable para que no se muestre la animación otra vez
-    animacionMostrada = true;
-    
-    // Esperar 10 segundos antes de cargar la página
-    setTimeout(function() {
-      window.location.href = "index.html";
-    }, 5000); // 5000 milisegundos (5 segundos) de espera antes de redireccionar
-  } else {
-    // Si la animación ya se ha mostrado, simplemente cargar la página de destino
-    window.location.href = "index.html";
-  }
+function showSlide(n) {
+if (n < 0) {
+n = sliderItems.length - 1;
+} else if (n >= sliderItems.length) {
+n = 0;
+}
+for (var i = 0; i < sliderItems.length; i++) {
+sliderItems[i].style.display = "none";
+}
+sliderItems[n].style.display = "flex";
+currentSlide = n;
+}
+
+function nextSlide() {
+showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+showSlide(currentSlide - 1);
+}
+
+var photoItems = document.querySelectorAll(".photo-item");
+
+function animatePhoto(photo) {
+photo.style.transform = "scale(1.1)";
+}
+
+function stopAnimatePhoto(photo) {
+photo.style.transform = "scale(1)";
 }
