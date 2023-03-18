de224ac2c6e52fbe184af9b19ec24de9845521d7
que veus tonto, ves cap allà <122235845+00TONI00@users.noreply.github.com>
2023-03-18 21:05:12 +0100

    hhhhh

--- java.js
+++ java.js
@@ -22,3 +22,29 @@ function prevSlide() {
 showSlide(currentSlide - 1);
 }
 
+
+
+
+
+
+
+var animacionMostrada = false;
+
+function cargarPagina() {
+  if (!animacionMostrada) {
+    // Mostrar la pantalla de espera con la animación
+    document.getElementById("pantallaEspera").style.display = "block";
+    
+    // Cambiar la variable para que no se muestre la animación otra vez
+    animacionMostrada = true;
+    
+    // Cargar la página a la que se quiere ir después de un breve lapso de tiempo
+    setTimeout(function() {
+      window.location.href = "https://otro-sitio.com";
+    }, 2000); // 2000 milisegundos (2 segundos) de espera antes de redireccionar
+  } else {
+    // Si la animación ya se ha mostrado, simplemente cargar la página de destino
+    window.location.href = "https://otro-sitio.com";
+  }
+}
+
